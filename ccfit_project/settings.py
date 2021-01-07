07fe5352bd5887ce9916a25c9e2e938d1a6dcd64
Jackson Ferreira <ferreirajackson.96@gmail.com>
2021-01-07 00:17:29 +0000

    deploy3

--- ccfit_project/settings.py
+++ ccfit_project/settings.py
@@ -147,8 +147,8 @@ LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'ca2mailer2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'CCT1234!'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
